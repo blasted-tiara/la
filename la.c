607f266e09e44cd33709804e9517eeea96a74464
blasted-tiara <sectioaurea13@gmail.com>
2024-01-21 14:51:18 +0100

    Ant now looks awesome!

--- la.c
+++ la.c
@@ -3,7 +3,9 @@
 #include <unistd.h>
 #include "la.h"
 
-int add_D51(int x);
+int add_ant(int x);
+int add_quote1(void);
+int show_ant_quote1(bool);
 int my_mvaddstr(int y, int x, char *str);
 
 int my_mvaddstr(int y, int x, char *str)
@@ -28,9 +30,16 @@ int main(int argc, char *argv[])
     scrollok(stdscr, FALSE);
 
     for (x = COLS - 1; ; --x) {
-        if (add_D51(x) == ERR) break;
+        if (add_ant(x) == ERR) break;
+        if (add_quote1() == ERR) break;
         if (x == COLS / 2 - ANT1LENGTH / 2) {
-            sleep(2);
+            show_ant_quote1(true);
+            getch();
+            refresh();
+            sleep(3);
+            show_ant_quote1(false);
+            getch();
+            refresh();
         }
         getch();
         refresh();
@@ -43,7 +52,7 @@ int main(int argc, char *argv[])
 }
 
 
-int add_D51(int x)
+int add_ant(int x)
 {
     static char *d51[ANT1PATTERNS][ANT1HEIGHT + 1]
       = {
@@ -55,10 +64,63 @@ int add_D51(int x)
     int y, i;
 
     if (x < - ANT1HEIGHT)  return ERR;
-    y = LINES / 2 - 2;
+    y = LINES / 2 + 5;
 
     for (i = 0; i <= ANT1HEIGHT; ++i) {
         my_mvaddstr(y + i, x, d51[(ANT1LENGTH + x) % ANT1PATTERNS][i]);
+    }
+
+    return OK;
+}
+
+int add_quote1(void) {
+    static char *quote1[QUOTE1_HEIGHT] = {
+        QUOTE1_BORDER,
+        QUOTE1_TEXT1,
+        QUOTE1_TEXT2,
+        QUOTE1_BORDER
+    };
+    
+    int i;
+    
+    for (i = 0; i < QUOTE1_HEIGHT; ++i) {
+        my_mvaddstr(LINES / 2 - 10 + i, COLS / 2 - 25, quote1[i]);
+    }
+    
+    return OK;
+}
+
+int show_ant_quote1(bool show) {
+    int y = LINES / 2;
+    int x = COLS / 2 - 15;
+    if (show) {
+        static char *ant_quote1[ANT_QUOTE1_HEIGHT] = {
+            ANT_QUOTE1_TOP,
+            ANT_QUOTE1_TEXT,
+            ANT_QUOTE1_BOT1,
+            ANT_QUOTE1_BOT2,
+            ANT_QUOTE1_BOT3
+        };
+        
+        int i;
+        
+        for (i = 0; i < ANT_QUOTE1_HEIGHT; ++i) {
+            my_mvaddstr(y + i, x, ant_quote1[i]);
+        }
+    } else {
+        static char *ant_quote_del[ANT_QUOTE1_HEIGHT] = {
+            ANT_QUOTE_DEL,
+            ANT_QUOTE_DEL,
+            ANT_QUOTE_DEL,
+            ANT_QUOTE_DEL,
+            ANT_QUOTE_DEL
+        };
+        
+        int i;
+        
+        for (i = 0; i < ANT_QUOTE1_HEIGHT; ++i) {
+            my_mvaddstr(y + i, x, ant_quote_del[i]);
+        }
     }
 
     return OK;
--- la.h
+++ la.h
@@ -7,10 +7,25 @@
 #define ANT1STR3 "  (\" )(_..._) "
 
 #define ANT1LGS1 "   ^^  \\\\  \\\\ "
-
 #define ANT1LGS2 "   ^^  /\\  /\\ "
-
 #define ANT1LGS3 "   ^^  //  // "
 
-
 #define ANT1DEL  "               "
+
+#define QUOTE1_HEIGHT   4
+#define QUOTE1_WIDTH    56
+
+#define QUOTE1_BORDER   "+------------------------------------------------------+"
+#define QUOTE1_TEXT1    "| Seeing the ant march unwaveringly across the screen, |"
+#define QUOTE1_TEXT2    "|           it fills you with determination.           |"
+
+#define ANT_QUOTE1_LENGTH   24
+#define ANT_QUOTE1_HEIGHT   5
+
+#define ANT_QUOTE1_TOP      "  /(\"\")\\~~/(\"\")\\  "
+#define ANT_QUOTE1_TEXT     " (  Keep going! )       "
+#define ANT_QUOTE1_BOT1     "  (~.~.-.   .~.)        "
+#define ANT_QUOTE1_BOT2     "         \\ )           "
+#define ANT_QUOTE1_BOT3     "          V             "
+
+#define ANT_QUOTE_DEL       "                        "
